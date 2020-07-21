ba2b4bbd6f5b6ac083e7d48c214279f1cf4944f2
nuttyguy360 <smsovereign3@gmail.com>
2020-07-20 20:49:18 -0600

    Refactored more code. Added catched to errors. For some reason the console refreshes at the end of the code and I can't see output

--- pyproc.py
+++ pyproc.py
@@ -8,11 +8,12 @@ import os.path as op
 import requests
 import json
 import pandas as pd
+import time
 
 def find_summoner():
   if(op.isfile('users.txt')):
   
-    use_name = input('Would you like to check a saved user? (y/n)\n')
+    use_name = input('Would you like to check a saved user? (y/n)\n (Any other input will quit)')
     
     if(use_name == 'y'):
       print('Enter the number next to the name you want to use')
@@ -30,18 +31,24 @@ def find_summoner():
       f.seek(0)        
       names = f.readlines()
       summoner_name = names[int(select)-1]
+      summoner_name = summoner_name.rstrip()
       f.close
-    
+    if(use_name == 'n'):
+        new_inp = input('Would you like to quit(q) or enter a new summoner name(n)?')
+        if(new_inp == 'q'):
+            quit()
+        elif(new_inp == 'n'):
+            summoner_name = input('Enter your summoner name:\n')
     else:
-        summoner_name = input('Enter your summoner name:\n')
-    
+        quit()
   return summoner_name
+
 
 def save_summoner(summoner_name):
   with open('users.txt') as users:
     names = users.readlines()
     for name in names:
-      if name == summoner_name:
+      if name.rstrip() == summoner_name:
         return
   
   save_name = input('Would you like to store this user? (y/n)\n')
@@ -50,7 +57,7 @@ def save_summoner(summoner_name):
     f.write(summoner_name + '\n')
     f.close
     
-def validate_game(match):
+def validate_game(match):    
     return match['gameVersion'][:5] == '10.14'
         
     
@@ -71,12 +78,48 @@ def show_result(takedowns):
     else:
         remain = 350 - takedowns
         print('Not there yet! You need %d more takedowns. Keep playing match-made games!' % (remain))
+        
+def validate_request(request):
+    try:
+        code = request['status']['status_code']
+        if(code == 403):
+            print('Error while trying to complete request:')
+            print(code, ':', request['status']['message'])
+            print('Most likely cause: Bad API key')
+            return 403
+        if(code == 404):
+            print('Error while trying to complete request:')
+            print(code, ':', request['status']['message'])
+            print('Most likely cause: Bad summoner name')
+            return 404
+        if(code == 429):
+            print('Error while trying to complete request:')
+            print(code, ':', request['status']['message'])
+            print('Most likely cause: Rate limit exceeded')
+            return 429
+    except:
+        return 0
+    
+def wait_for_api(wait_time):
+    inp = ' '
+    while (inp != 'w' or inp != 'q'):
+        inp = input('Would you like to wait(w)), or end the program now?(q)')        
+    if(inp == 'w'):
+          print('Waiting for API limit to reset...')
+          time_to_wait = wait_time
+          while time_to_wait > 0:
+                print('Time: %d seconds' % (time_to_wait))
+                time.sleep(1)
+                time_to_wait -= 1
+    if(inp == 'q'):
+        quit()
+    
 '''
 Main starts here
 '''
 
-print('Note: Do the API limitiations, this app can currently only process',
-      'the last 80 matchmade, SR games played. As such the result may be',
+print('Note: Due the API limitiations, this app can currently only process',
+      'the last 80 matchmade, SR games played. As such, the result may be',
       'inaccurate')
 region_url = 'https://na1.api.riotgames.com/'
 name_get = "lol/summoner/v4/summoners/by-name/"
@@ -90,18 +133,28 @@ summoner_name = find_summoner()
 validName = False
 while validName != True:
 
-  request_string = region_url + name_get + summoner_name + api_key
-  request = requests.get(request_string)
-  
-  try:
+    request_string = region_url + name_get + summoner_name + api_key
+    request = requests.get(request_string)
     request_json = json.loads(request.text)
-    validName = True
-  except:
-    print("Invalid username\n")
-    summoner_name = input('Enter your summoner name:\n')
+    
+    valid_code = validate_request(request_json)
+    
+    if(valid_code == 403):
+        api_inp =  input("Enter the api key:\n")
+        api_key = api_url + api_inp
+    if(valid_code == 404):
+        print("Name input: '%s'" % (summoner_name))
+        summoner_name = find_summoner()
+    if(valid_code == 429):
+        wait_for_api(2 * 60)
+    if(valid_code == 0):
+        validName = True
+        
+
 
 save_summoner(summoner_name)
 #print(request_json)
+
 
 account_id = request_json['accountId']
 
@@ -129,9 +182,13 @@ for game in matchlist_df['gameId']:
   match_request = requests.get(match_info_url + str(game) + api_key)
   match_json = json.loads(match_request.text)
   
+  valid_code = validate_request(match_json)
+  if(valid_code == 429):
+      wait_for_api(2 * 60)
+  
   if(validate_game(match_json) != True):
       num_game += 1
-      print('Total takedowns so far: %d/350' % (total_takedown))
+      print('Total takedowns so far: %d/350' % (total_takedown), flush=True)
       continue
 
   partDto_df = pd.DataFrame(match_json['participants'])
@@ -149,7 +206,7 @@ for game in matchlist_df['gameId']:
   participant = partDto_df[partDto_df['participantId'] == partId]
   #print(participant.to_string())
   total_takedown += count_match(participant)
-  print('Total takedowns so far: %d/350' % (total_takedown))
+  print('Total takedowns so far: %d/350' % (total_takedown), flush=True)
   if(total_takedown >= 350):
       break
   num_game += 1
