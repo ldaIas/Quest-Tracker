38a59a887459593a02a96a5e66a84bbe12e61df7
nuttyguy360 <smsovereign3@gmail.com>
2020-07-20 19:21:50 -0600

    Finished the processing of the project. Refactored several lines of code into their own functions. Applied a limit of 80 requests to try to not go over the API request limit.

--- pyproc.py
+++ pyproc.py
@@ -24,16 +24,16 @@ def find_summoner():
         name_ind += 1
         
       select = input()
-      while(int(select) > name_ind):
-        select = input('Please enter the number associated with the user to check')
+      while(int(select) > name_ind - 1):
+        select = input('Please enter the number associated with the user to check\n')
 
       f.seek(0)        
       names = f.readlines()
       summoner_name = names[int(select)-1]
       f.close
     
-  else:
-    summoner_name = input('Enter your summoner name:\n')
+    else:
+        summoner_name = input('Enter your summoner name:\n')
     
   return summoner_name
 
@@ -47,16 +47,41 @@ def save_summoner(summoner_name):
   save_name = input('Would you like to store this user? (y/n)\n')
   if save_name == 'y':
     f = open('users.txt', 'a')
-    f.write(summoner_name)
+    f.write(summoner_name + '\n')
     f.close
+    
+def validate_game(match):
+    return match['gameVersion'][:5] == '10.14'
+        
+    
+def count_match(participant):
+    
+    if( int(participant.get(key = 'spell1Id')) != 11 ) & ( int((participant.get(key = 'spell2Id')) != 11) ):
+        return 0
+    if( (participant['stats'][int(participant['participantId'])-1]['neutralMinionsKilled'] < 40 ) |
+       (participant['stats'][int(participant['participantId'])-1]['neutralMinionsKilled'] < participant['stats'][int(participant['participantId'])-1]['totalMinionsKilled'] )):
+           return 0
+    return participant['stats'][int(participant['participantId'])-1]['kills'] + participant['stats'][int(participant['participantId'])-1]['assists']
 
+def show_result(takedowns):
+    print('Takedowns: %d/350' % (takedowns))
+    if(takedowns >= 350):
+        print('Congrats! You should have the Lillia shard!\n',
+              'You can use it in 10.15')
+    else:
+        remain = 350 - takedowns
+        print('Not there yet! You need %d more takedowns. Keep playing match-made games!' % (remain))
 '''
 Main starts here
 '''
+
+print('Note: Do the API limitiations, this app can currently only process',
+      'the last 80 matchmade, SR games played. As such the result may be',
+      'inaccurate')
 region_url = 'https://na1.api.riotgames.com/'
 name_get = "lol/summoner/v4/summoners/by-name/"
 api_url = "?api_key="
-api_inp = input("Enter the api key:\n")
+api_inp = 'RGAPI-97dfa0b8-530c-406e-844a-0b8b1dcb48ec'# input("Enter the api key:\n")
 api_key = api_url + api_inp
 
 summoner_name = find_summoner()
@@ -77,41 +102,59 @@ while validName != True:
 
 save_summoner(summoner_name)
 #print(request_json)
-    
+
 account_id = request_json['accountId']
+
+
+
 matchlist_string = region_url + '/lol/match/v4/matchlists/by-account/'
-matchlist_req = requests.get(matchlist_string + account_id + api_key)
+queueFilter = {400, 420, 430, 440, 700, 830, 840, 850}
+params = {
+        'queue' : queueFilter,
+        'endIndex' : 80
+        }
+matchlist_req = requests.get( (matchlist_string + account_id + api_key),
+                             params)
 matchlist_json = json.loads(matchlist_req.text)
 #print(matchlist_json)
 matchlist_df = pd.DataFrame(matchlist_json['matches'])
-print(matchlist_df)
+#print(matchlist_df)
 
+total_takedown = 0
+num_game = 1
+print('Counting takedowns in games...')
 for game in matchlist_df['gameId']:
+  print('Game: %d/%d' % (num_game, matchlist_df['gameId'].size), end='.....')
   match_info_url = region_url + '/lol/match/v4/matches/'
   match_request = requests.get(match_info_url + str(game) + api_key)
   match_json = json.loads(match_request.text)
+  
+  if(validate_game(match_json) != True):
+      num_game += 1
+      print('Total takedowns so far: %d/350' % (total_takedown))
+      continue
 
   partDto_df = pd.DataFrame(match_json['participants'])
-
+  #print(partDto_df)
   partId_df = pd.DataFrame(match_json['participantIdentities'])
-  print(partId_df)
-  #player_series = partId_df['player'][x]['accountId']
-  #print(player_series)
+  #print(partId_df)
   
-  df = pd.crosstab(partId_df.participantId, partId_df.player)
-  print(df)
-  #player_series = partId_df['participantId'][x for x in partId_df['player']]
-  #print(player_series)
-  #player = player_series[0]['accountId'] 
-  #print(player)
-  #playeracc = player['accountId']
-  #print(playeracc)
-  
-  
-  
-  break
+  partId = 1
+  for player in partId_df['player']:
+      if player['accountId'] == account_id:
+          break
+      partId += 1
+      
+  #print(partId)
+  participant = partDto_df[partDto_df['participantId'] == partId]
+  #print(participant.to_string())
+  total_takedown += count_match(participant)
+  print('Total takedowns so far: %d/350' % (total_takedown))
+  if(total_takedown >= 350):
+      break
+  num_game += 1
 
-
+show_result(total_takedown)
 
 
 
